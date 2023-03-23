9b3edaf05ec79e1c3299755a19a5b8debf20e920
Ivan Rogério <ivanpassosfilho@gmail.com>
2023-03-23 14:43:55 -0300

    Adicionando comentários e importando CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// React
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// Component
+import { App } from './App';
+
+// CSS
+import './global.css';
+
+ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
