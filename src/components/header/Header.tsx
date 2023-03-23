9083fed4bba61dfb853f228b16acf693b9ccb2b0
Ivan Rogério <ivanpassosfilho@gmail.com>
2023-03-23 15:51:12 -0300

    Componente e estilos do Header criado e configurado

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,17 @@
+// CSS
+import style from './header.module.css';
+
+// Logo
+import igniteLogo from '../../assets/ignite-logo.svg';
+
+export const Header = () => {
+  return (
+    <header className={style.header}>
+      <img
+        src={igniteLogo}
+        alt='ignite-feed-logo'
+      />
+      <h1>Ígnite Feed</h1>
+    </header>
+  );
+};
